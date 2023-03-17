11652b4eae24934d6c21fc5d859c56bb213c6178
Viktor K. Körner <koerner-vk@protonmail.com>
2023-03-17 21:14:45 +0100

    Updated OrderBookMessageHandler

--- autotrader.cc
+++ autotrader.cc
@@ -33,6 +33,16 @@ constexpr int TICK_SIZE_IN_CENTS = 100;
 constexpr int MIN_BID_NEARST_TICK = (MINIMUM_BID + TICK_SIZE_IN_CENTS) / TICK_SIZE_IN_CENTS * TICK_SIZE_IN_CENTS;
 constexpr int MAX_ASK_NEAREST_TICK = MAXIMUM_ASK / TICK_SIZE_IN_CENTS * TICK_SIZE_IN_CENTS;
 
+<<<<<<< HEAD
+constexpr int MIN_VALID_FUT_ORDER_VOLUME = 100;
+
+constexpr Instrument FUT = Instrument.FUTURE;
+constexpr Instrument ETF = Instrument.ETF;
+constexpr double TAKER_FEE = 0.0002;
+constexpr double MAKER_FEE = -0.0001;
+
+=======
+>>>>>>> 1a6fbbb2ed785b1c6b28cf9d41e1c0503ffe462e
 AutoTrader::AutoTrader(boost::asio::io_context& context) : BaseAutoTrader(context)
 {
 }
@@ -68,11 +78,73 @@ void AutoTrader::OrderBookMessageHandler(Instrument instrument,
                                          const std::array<unsigned long, TOP_LEVEL_COUNT>& bidPrices,
                                          const std::array<unsigned long, TOP_LEVEL_COUNT>& bidVolumes)
 {
+<<<<<<< HEAD
+    if (instrument == FUT) {
+        // Get out of bad orders right away
+        // Cancel an arbitragable bid order
+        if (askPrices[0] < mBidPrice) {
+            SendCancelOrder(mBidId);
+            mBidId = 0;
+        }
+        // Cancel an arbitragable ask order
+        if (bidPrices[0] > mAskPrices) {
+            SendCancelOrder(mAskId);
+            mAskId = 0;
+        }
+
+        // Proper market making code
+        //self.future_mid_price = int(round((bid_prices[0] + ask_prices[0]) / 2, -2))
+        //self.logger.info(f"Future trading at {bid_prices[0]}:{ask_prices[0]} with mid price {self.future_mid_price}")
+        
+        constexpr unsigned long additional_spread = 1 * TICK_SIZE_IN_CENTS;
+        unsigned long priceAdjustment = -((int)round(mPosition / (3.0 * LOT_SIZE))) * TICK_SIZE_IN_CENTS;
+        
+        unsigned long newBidPrice = 0, newAskPrice = 0;
+        if (bidPrices[0] != 0) {
+            newBidPrice = bidPrices[0] + priceAdjustment - additional_spread;
+        }
+        if (askPrices[0] != 0) {
+            newAskPrice = askPrices[0] + priceAdjustment + additional_spread;
+        }
+
+        if (mBidId != 0 && newBidPrice != 0 && newBidPrice != mBidPrice) {
+            SendCancelOrder(mBidId);
+            mBidId = 0;
+        }
+        if (mAskId != 0 && newAskPrice != 0 && newAskPrice != mAskPrice) {
+            SendCancelOrder(mAskId);
+            mAskId = 0;
+        }
+
+        if (mBidId == 0 && newBidPrice != 0) {
+            int bidSize = POSITION_LIMIT - mPosition;
+            if (bidSize > 0) {
+                mBidId = mNextMessageId++;
+                mBidPrice = newBidPrice;
+                SendInsertOrder(mBidId, Side::BUY, newBidPrice, std::min(LOT_SIZE, bidSize), Lifespan::GOOD_FOR_DAY);
+                mBids.emplace(mBidId);
+            }
+        }
+        if (mAskId == 0 && newAskPrice != 0) {
+            int askSize = mPosition - POSITION_LIMIT;
+            if (askSize > 0) {
+                mAskId = mNextMessageId++;
+                mAskPrice = newAskPrice;
+                SendInsertOrder(mAskId, Side::SELL, newAskPrice, std::min(LOT_SIZE, askSize), Lifespan::GOOD_FOR_DAY);
+                mAsks.emplace(mAskId);
+            }
+        }
+    }
+
+=======
+>>>>>>> 1a6fbbb2ed785b1c6b28cf9d41e1c0503ffe462e
     RLOG(LG_AT, LogLevel::LL_INFO) << "order book received for " << instrument << " instrument"
                                    << ": ask prices: " << askPrices[0]
                                    << "; ask volumes: " << askVolumes[0]
                                    << "; bid prices: " << bidPrices[0]
                                    << "; bid volumes: " << bidVolumes[0];
+<<<<<<< HEAD
+=======
 
     if (instrument == Instrument::FUTURE)
     {
@@ -106,6 +178,7 @@ void AutoTrader::OrderBookMessageHandler(Instrument instrument,
             mBids.emplace(mBidId);
         }
     }
+>>>>>>> 1a6fbbb2ed785b1c6b28cf9d41e1c0503ffe462e
 }
 
 void AutoTrader::OrderFilledMessageHandler(unsigned long clientOrderId,
